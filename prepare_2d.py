0b5de53cb378fa32ce0bde555eb7ef7594842572
ozeuth <ozeuthgatum@gmail.com>
2020-05-27 16:20:09 +0800

    Synchronicity issue fixed

--- prepare_2d.py
+++ prepare_2d.py
@@ -1,7 +1,6 @@
-import os.path
-import time
 import PIL
 from PIL import Image, ImageDraw
+
 node = hou.pwd()
 geo = node.geometry()
 boundaries = node.inputs()[1].geometry().pointGroups()
@@ -12,7 +11,7 @@ if (pix):
   i = 0
   for boundary in boundaries:
     if i == 0:
-      i+= 1
+      i += 1
       continue
     pix_pos = []
     points = boundaries[i].points()
@@ -20,11 +19,10 @@ if (pix):
       pix_attrib = point.attribValue(pix)
       pix_point = (pix_attrib[(i-1) * 3], pix_attrib[(i-1) * 3 + 1], pix_attrib[(i-1) * 3 + 2])
       pix_pos.append(pix_point)
-    while (os.path.isfile(path_name + "/opening_" + str(i) + ".png.mantra_checkpoint")):
-      time.sleep(1) # BUG: Still does not wait for image to be rendered
     image = Image.open(path_name + "/opening_" + str(i) + ".png")
     draw = ImageDraw.Draw(image)
     for j in range(len(points)):
       draw.point((int(pix_pos[j][0]), int(pix_pos[j][1])), "red")
     image.save(path_name + "/opening_" + str(i) + ".png")
     i += 1
+node.bypass(True)
--- session.py
+++ session.py
@@ -1,23 +1,36 @@
 # -- Houdini Mesh Repairer -- #
 import hou
 import nodesearch
+import os
+import time
 
 def repair():
+  node_3d = None
+  node_2d = None
   '''
   1-3: Choose 3D Context Region
   '''
   matcher = nodesearch.Name("mesh_repairer")
   for node in matcher.nodes(hou.node("/obj/"), recursive=True):
     if hou.node(node.path() + "/prepare_3d"):
-      hou.node(node.path() + "/prepare_3d").bypass(False)
+      node_3d = hou.node(node.path() + "/prepare_3d")
+    if hou.node(node.path() + "/prepare_2d"):
+      node_2d = hou.node(node.path() + "/prepare_2d")
+  assert (node_3d and node_2d), ("ERROR: Please reinstate Digital Asset")
+  node_3d.bypass(False)
   '''
   4. Generate 2D Render
   '''
+  image_paths = []
   renders = hou.nodeType(hou.ropNodeTypeCategory(), "ifd").instances()
   for render in renders:
     if "oz_render_" in render.name():
+      image_paths.append(render.parm("vm_picture").eval())
       render.render()
-
   '''
   5. Map 3D Context Region -> 2D Render
   '''
+  for image_path in image_paths:
+    while (os.path.isfile(image_path + ".mantra_checkpoint")):
+      time.sleep(1)
+  node_2d.bypass(False)
