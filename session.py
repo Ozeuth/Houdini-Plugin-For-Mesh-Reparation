438ecd5f6b00bba2ea1c937774bdc39520372101
ozeuth <ozeuthgatum@gmail.com>
2020-06-01 17:59:58 +0800

    Fixed rendering synchronicity issue

--- session.py
+++ session.py
@@ -3,7 +3,33 @@ import glob
 import hou
 import nodesearch
 import os
+from PIL import ImageFont
 import time
+from threading import Thread
+
+def render_then_map(image_paths, node_2d):
+  '''
+  NOTE: This is a tenuous solution to a synchronicity issue.
+  Absence of a rendering checkpoint file means:
+    A. Rendering has ended
+    OR
+    B. Rendering has not started
+  We must first check that all renders have started, then that all renders have ended
+  This must be done in a thread (Or renders that have not started will not start)
+  '''
+  render_started = [False] * len(image_paths)
+  while sum(render_started) != len(image_paths):
+    for i in range(len(image_paths)):
+      render_started[i] = render_started[i] or os.path.isfile(image_paths[i] + ".mantra_checkpoint")
+      if not (render_started[i] or os.path.isfile(image_paths[i] + ".mantra_checkpoint")):
+        print("Waiting to start" + str(image_paths[i]))
+    time.sleep(1)
+
+  for image_path in image_paths:
+    while (os.path.isfile(image_path + ".mantra_checkpoint")):
+      print("Waiting for render complete " + image_path)
+      time.sleep(1)
+  node_2d.bypass(False)
 
 def repair():
   node_3d = None
@@ -43,11 +69,11 @@ def repair():
   '''
   5. Map 3D Context Region -> 2D Render
   '''
-  for image_path in image_paths:
-    while (os.path.isfile(image_path + ".mantra_checkpoint")):
-      time.sleep(1)
-  node_2d.bypass(False)
-
-  # Clean up
+  #render_then_map(image_paths, node_2d)
+  render_map_thread = Thread(target=render_then_map, args=(image_paths,node_2d,))
+  render_map_thread.start()
+  '''
+  *. Clean-Up
+  '''
   for node in mark_for_destroy:
     node.destroy()
