f151dc3cd27077d34c63cdac58d9d802374ddf58
ozeuth <ozeuthgatum@gmail.com>
2020-06-12 15:23:36 +0800

    progress on 2d inpainting

--- prepare_2d.py
+++ prepare_2d.py
@@ -1,5 +1,8 @@
 import PIL
 import hou
+import math
+import numpy as np
+import sys
 from PIL import Image, ImageDraw
 
 node = hou.pwd()
@@ -13,7 +16,7 @@ is_alpha = bool(hou.session.find_parm(hou.parent(), "isAlpha"))
 '''
  8. Map 3D Context Region -> 2D Render
   Generate Mask and Conditioning Area combined image from mapping,
-  ALL_x,y mask_conditioning_sub[x, y] = (255, 0, 0)*1_M + (0, 255, 0)*1_C + (0, 0, 255)*1_W + (0, 0, 0)
+  ALL_x,y mcw[x, y] = (255, 0, 0)*1_M + (0, 255, 0)*1_C + (0, 0, 255)*1_W + (0, 0, 0)
   where
     1_M = 1 if in Mask else 0
     1_C = 1 if in Conditioning Area else 0
@@ -73,8 +76,8 @@ if (pix):
     image = Image.open(path_name + "/opening_" + str(i) + ".png")
     image_size = image.size
     image.close()
-    mask_cond = Image.new('RGB', image_size, color=(0, 0, 255))
-    draw = ImageDraw.Draw(mask_cond)
+    mcw = Image.new('RGB', image_size, color=(0, 0, 255))
+    draw = ImageDraw.Draw(mcw)
     edge_pixels = []
     for edge in edges:
       edge_points = edge.points()
@@ -86,10 +89,63 @@ if (pix):
     draw.line(edge_pixels, fill=(0, 255, 255), width= 2*w + 1)
     draw.line((edge_pixels[0], edge_pixels[len(edge_pixels)-1]), fill=(0, 255, 255), width= 2*w + 1)
     draw.polygon(edge_pixels, fill=(255, 0, 0), outline=None)
-    mask_cond.save(path_name + "/mask_cond_w_" + str(i) + ".png")
-    mask_cond.close()
-'''
-9. 2D inpainting
-'''
+    mcw.save(path_name + "/mcw_" + str(i) + ".png")
+    mcw.close()
+  
+  '''
+  9. 2D inpainting
+    We follow B Galerne, A Leclaire[2017],
+      inpainting using Gaussian conditional simulation, relying on a Kriging framework
+  '''
+  for i in range(1, len(boundaries)):
+    image = Image.open(path_name + "/opening_" + str(i) + ".png")
+    image = np.array(image)
+    image_size = image.shape
+    mcw = Image.open(path_name + "/mcw_" + str(i) + ".png")
+    mcw = np.array(mcw)
+    epsilon = 0.001
+    '''
+    9A. Compute
+      v_het = 1/|w|*[SUMr_elem(w)(v(r)), SUMg_elem(g)(v(g)), SUMb_elem(b)(v(b))]
+            | 1/sqrt(|w|)*(v-v_het)
+      t_v = | 0 otherwise
+        where
+          v = w restricted to u (w where u is opaque)
+    '''
+    v = np.ma.masked_where(np.where(np.logical_and(mcw[:,:,[2]] == 255, image[:,:,[3]] > epsilon), [1, 1, 1, 1], [0, 0, 0, 0])==0, image).filled(fill_value=0)
+    v_num = np.sum(np.where(v[:,:,[3]] != 0, 1, 0))
+    v_het = [0, 0, 0, 0]
+    for d in range(3):
+      v_het[d] = np.sum(v[:, :, d])
+    v_het = v_het / v_num
 
+    t_v = np.zeros(v.shape)
+    t_v[:,:,...] = (np.maximum((v - v_het), t_v) / math.sqrt(v_num)) * 255
+    t_v[:,:,[3]] = v[:,:,[3]]
+    '''
+    9B. Draw Gaussian Sample,
+      F = t_v * W
+      where
+        W = Gaussian White Noise
+    '''
+    W_mean = 0
+    W_var = 1
+    W_same = True
+
+    if (W_same):
+      # Same random value per color channel, as intended
+      W = np.random.normal(W_mean, W_var, (image_size[0], image_size[1], 1)).astype(np.float32)
+      W = np.concatenate((W, W, W), axis=2)
+    else:
+      # Different random value per channel, experimental
+      W = np.random.normal(W_mean, W_var, image_size)
+
+    #print(t_v.shape)
+    #print(W.shape)
+    #F = np.convolve(t_v, W)
+    #W *= 255
+    if i ==1:
+      im = Image.fromarray(np.uint8(F))
+      im.save(path_name + "/test.png")
+      im.close()
 node.bypass(True)
