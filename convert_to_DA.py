9355d3307b986376857a10461a10749c8e17f495
ozeuth <ozeuthgatum@gmail.com>
2020-09-02 18:19:15 +0800

    Change to Digital Asset

--- convert_to_DA.py
+++ convert_to_DA.py
@@ -1,26 +1,15 @@
-node = hou.pwd() 
-geo = node.geometry()
-geometry_name = node.parent().name()
-path = "/obj/" + geometry_name + "/"
+import hou
 
-bbox = hou.node(path + "oz_bbox")
-uv_bbox = hou.node(path + "uv_viewer_bbox")
-uv_viewer_all = hou.node(path + "uv_viewer_all")
-uv_viewer = hou.node(path + "uv_viewer")
-group_1 = hou.node(path + "find_border")
-group_2 = hou.node(path + "view_borders")
-group_3 = hou.node(path + "edge_border")
-py_3d = hou.node(path + "prepare_3d")
-py_op_3d = hou.node(path + "optimize_3d")
-vex_clean = hou.node(path + "clean_3d_to_2d")
-vex_1 = hou.node(path + "3d_to_2d_1")
-vex_2 = hou.node(path + "3d_to_2d_2")
-vex_ratio = hou.node(path + "3d_to_2d_ratio")
-py_2d = hou.node(path + "prepare_2d")
-output = hou.node(path + "output")
+node = hou.pwd()
+path = "/obj/" + node.parent().name() + "/"
+file_node = hou.node(path + "mesh_to_repair")
 
-subnet = node.parent().collapseIntoSubnet((bbox, uv_bbox, uv_viewer_all, group_1, group_2, group_3, uv_viewer, py_3d, vex_1, py_op_3d, vex_clean, vex_2, vex_ratio, py_2d, output), "mesh_repairer")
+DA_nodes = []
+for curr_node in node.parent().children():
+  if curr_node != node and curr_node != file_node:
+    DA_nodes.append(curr_node)
 
+subnet = node.parent().collapseIntoSubnet(tuple(DA_nodes))
 if subnet.canCreateDigitalAsset():
   asset = subnet.createDigitalAsset(
     name="Mesh_Repairer_Oz",
@@ -30,17 +19,55 @@ if subnet.canCreateDigitalAsset():
   asset.layoutChildren()
   parm_group = asset.parmTemplateGroup()
 
-  inputs_folder = hou.FolderParmTemplate("inputs_folder", "Inputs", folder_type = hou.folderType.Tabs)
+  # Input Folder
+  inputs_folder = hou.FolderParmTemplate("inputs_folder", "Inputs", folder_type=hou.folderType.Tabs)
   inputs_folder.addParmTemplate(hou.StringParmTemplate("repairPath", "Repairer Path", 1, help="Path to Mesh Repairer", script_callback='if not ("# -- Houdini Mesh Repairer -- #" in hou.sessionModuleSource()): hou.appendSessionModuleSource(open(hou.pwd().parm("repairPath").eval() + "/session.py", "r").read())', script_callback_language = hou.scriptLanguage.Python))
-  inputs_folder.addParmTemplate(hou.ButtonParmTemplate("new", "New Reparation", script_callback = "hou.session.repair()", script_callback_language = hou.scriptLanguage.Python, help="Begin New Reparation"))
+  inputs_folder.addParmTemplate(hou.ButtonParmTemplate("new", "Full Reparation", script_callback = "hou.session.repair()", script_callback_language = hou.scriptLanguage.Python, help="Begin New Reparation"))
+  # Low Frequency Folder
+  low_folder = hou.FolderParmTemplate("low folder", "Low Frequency", folder_type = hou.folderType.Tabs)
+  low_alpha_beta = hou.FloatParmTemplate("low_alpha_beta", "Corrected Normal Ratio, Alpha:Beta", 
+    1, default_value=(0.5,), min=0.0, max=1.0, min_is_strict=True, max_is_strict=True,
+    help="Try larger alpha for rounder output topologies, larger beta for flatter output topologies")
+  low_w1_w2 = hou.FloatParmTemplate("low_w1_w1", "Optimal Position Ratio, w1:w2",
+    1, default_value=(0.5,), min=0.0, max=1.0, min_is_strict=True, max_is_strict=True,
+    help="Controls learning of optimized new point positions")
+  low_folder.addParmTemplate(low_alpha_beta)
+  low_folder.addParmTemplate(low_w1_w2)
 
+  low_folder.addParmTemplate(hou.ToggleParmTemplate("isIter", "Use Iteration Threshold", 0, help="Check to limit number of iterations of AFT"))
+  low_iter_threshold = hou.IntParmTemplate("low_iter_threshold", "Iteration Threshold", 
+    1, default_value=(2000,), min=0, max=3000,
+    min_is_strict=True, max_is_strict=False, 
+    help="Maximum number of iterations AFT can run for")
+  low_iter_threshold.setConditional(hou.parmCondType.DisableWhen, "{ isIter == 0 }")
+  low_folder.addParmTemplate(low_iter_threshold)
+  low_folder.addParmTemplate(hou.ToggleParmTemplate("isAngle", "Use Angle Threshold", 1, help="Check to allow randomized AFT generation"))
+  low_angle_threshold = hou.IntParmTemplate("low_angle_threshold", "Angle Threshold", 
+    1, default_value=(140,), min=0, max=360,
+    min_is_strict=True, max_is_strict=True, 
+    help="Maximum angle for a boundary point to be considered for AFT randomized selection")
+  low_angle_threshold.setConditional(hou.parmCondType.DisableWhen, "{ isAngle == 0 }")
+  low_folder.addParmTemplate(low_angle_threshold)
+  low_folder.addParmTemplate(hou.ToggleParmTemplate("isDistance", "Use Distance Threshold", 1, help="Check to allow merger of new AFT points by distance"))
+  low_distance_threshold = hou.FloatParmTemplate("low_distance_threshold", "Distance Threshold",
+    1, default_value=(0.25,), min=0.0, max=1.0,
+    min_is_strict=True, max_is_strict=True,
+    help="Maximum proportion of distance to another point to be considered for AFT point merger")
+  low_distance_threshold.setConditional(hou.parmCondType.DisableWhen, "{ isDistance == 0 }")
+  low_folder.addParmTemplate(low_distance_threshold)
+  low_folder.addParmTemplate(hou.ButtonParmTemplate("low_new", "Low Frequency Reparation", script_callback = "hou.session.low_repair()", script_callback_language = hou.scriptLanguage.Python, help="Begin New Low-Frequency Reparation"))
 
-  mapping_3d_to_2d_folder = hou.FolderParmTemplate("3d_to_2d_folder", "3D to 2D", folder_type = hou.folderType.Tabs)
-  mapping_3d_to_2d_folder.addParmTemplate(hou.ToggleParmTemplate("isAlpha", "Change Alpha", 0, help="Check if you wish to use a different alpha to determine optimal occupancy ratio"))
+  # High Frequency Folder
+  high_folder = hou.FolderParmTemplate("high_folder", "High Frequency", folder_type = hou.folderType.Tabs)
+  high_folder.addParmTemplate(hou.ToggleParmTemplate("isAlpha", "Change Alpha", 0, help="Check if you wish to use a different alpha to determine optimal occupancy ratio"))
   alpha = hou.FloatParmTemplate("alpha", "Occupancy Ratio Alpha", 1, default_value=(0.87,), min=0.0, max=1.0, min_is_strict=True, max_is_strict=True, help="alpha used in S Salamanca,P Merchan,A Adan,E Perez,C Cerrada[2008]")
   alpha.setConditional(hou.parmCondType.DisableWhen, "{ isAlpha == 0 }")
-  mapping_3d_to_2d_folder.addParmTemplate(alpha)
+  high_folder.addParmTemplate(alpha)
+  high_folder.addParmTemplate(hou.ButtonParmTemplate("high_new", "High Frequency Reparation", script_callback = "hou.session.high_repair()", script_callback_language = hou.scriptLanguage.Python, help="Begin New High-Frequency Reparation"))
 
   parm_group.append(inputs_folder)
-  parm_group.append(mapping_3d_to_2d_folder)
+  parm_group.append(low_folder)
+  parm_group.append(high_folder)
   asset.setParmTemplateGroup(parm_group)
+
+node.bypass(True)
