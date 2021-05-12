e41153ea1e3d6a32150836a664a7029e31d8a454
Ozeuth <ozeuthgatum@gmail.com>
2021-05-12 17:51:52 +0100

    Basic gap merger complete

--- gap_close.py
+++ gap_close.py
@@ -47,11 +47,11 @@ def min_dist_and_elem(point, virtual_edges, init=False):
   min_dist, min_elem = float('inf'), None
   for virtual_edge in virtual_edges:
     '''
-            p1
-            | proj
-      p_inter|------pi
-            |
-            p2
+          p1
+           | proj
+    p_inter|------pi
+           |
+          p2
     '''
     p1, p2 = virtual_edge
     if init:
@@ -95,43 +95,74 @@ for point in points:
   points_to_elem[point] = min_elem
 
 break_point  = 0
-marked_for_delete = []
-while not dist_to_pairs.empty() and break_point < 4:
+marked_for_delete_points, marked_for_delete_polys = [], []
+while not dist_to_pairs.empty() and break_point < 50:
   dist, pair = dist_to_pairs.get()
   point, elem = pair.point, pair.elem
   #if point not in marked_for_delete and elem not in marked_for_delete and points_to_elem[point] == elem: 
-  if point not in marked_for_delete and points_to_elem[point] == elem: 
+  if point not in marked_for_delete_points and points_to_elem[point] == elem: 
     invalid_edge = False
     if type(elem) == hou.Edge: # Point to Edge Contraction
       continue
     if type(elem) == hou.Point or invalid_edge: # Point to Point Contraction
-      if sort_points((point, elem)) in virtual_edges: # Edge Contraction
-        continue
-      else: # Non-Edge Contraction
-        print((point, elem))
-        elem.setPosition((elem.position() + point.position()) / 2)
-        # Update edge set
-        old_elem_edges = []
-        for p in points_neighbors[elem]:
-          old_elem_edges.append(sort_points((elem, p)))
+      elem.setPosition((elem.position() + point.position()) / 2)
+      for prim in point.prims():
+        if prim.type() == hou.primType.Polygon:
+          if elem not in prim.points():
+            prim.addVertex(elem)
+          else:
+            marked_for_delete_polys.append(prim)
+      marked_for_delete_points.append(point)
 
-        old_point_edges = []
-        new_point_edges = []
-        for p in points_neighbors[point]:
-          old_point_edges.append(sort_points((point, p)))
+      old_elem_edges = []
+      for p in points_neighbors[elem]: old_elem_edges.append(sort_points((elem, p)))
+      old_point_edges, new_point_edges = [], []
+      for p in points_neighbors[point]:
+        old_point_edges.append(sort_points((point, p)))
+        if p != elem:
           new_point_edges.append(sort_points((elem, p)))
+      
+      affected_elems, point_other = [], None
+      if sort_points((point, elem)) in virtual_edges:
+        print("E: " + str((point.number(), elem.number())))
+        ''' Point-Point Contraction: Edge Contraction
+        p1----point----elem----p2  =>   p1-----elem-----p2
+
+        Duplicate edges contain the contracted edge, e_point_elem. These must be:
+          1. removed from virtual edges
+          2. Update points_neighbors for p1 and elem
+          3. Update affected elems of point, elem, e_point_elem, e_p1_point, e_p2_elem
         '''
-        Duplicate edges indicate a seam has been closed. These must be:
+        duplicate_edge = sort_points((point, elem))
+        virtual_edges = (set(virtual_edges) - set(old_point_edges)).union(set(new_point_edges)) - set([duplicate_edge])
+
+        for neighbor in points_neighbors[point]:
+          points_neighbors[neighbor].remove(point)
+          if neighbor != elem:
+            points_neighbors[neighbor].append(elem)
+        points_neighbors[point].remove(elem)
+        points_neighbors[elem] += points_neighbors[point]
+        del points_neighbors[point]
+
+        affected_elems += old_point_edges + old_elem_edges + [point, elem]
+      else:
+        print("NE: " + str((point.number(), elem.number())))
+        duplicate_edges = set(new_point_edges).intersection(set(old_elem_edges))
+        ''' Point-Point Contraction: Non-Edge Contraction
+               / elem        
+              /           =>  other_______point
+             /            
+        other------- point
+
+        Duplicate edges e_elem_other, indicate a seam between e_elem_other, e_point_other
+        has been closed. These must be:
           1. removed from virtual edges
           2. have the other point removed from points_to_elem
           3. have the other point removed from points_neighbors
-          4. Update affected elems of other points
+          4. Update affected elems of the other point
         '''
-        point_other = None
-        affected_elems = []          
         duplicate_edges = (set(virtual_edges) - set(old_point_edges)).intersection(set(new_point_edges))
         virtual_edges = (set(virtual_edges) - set(old_point_edges)).union(set(new_point_edges)) - duplicate_edges
-
         if len(duplicate_edges) != 0:
           p1, p2 = list(duplicate_edges)[0]
           point_other = p1 if p2 == elem else p2
@@ -139,37 +170,31 @@ while not dist_to_pairs.empty() and break_point < 4:
           del points_neighbors[point_other]
           points_neighbors[point].remove(point_other)
           points_neighbors[elem].remove(point_other)
-          affected_elems = [point_other]
+          affected_elems += [point_other]
 
-        # Update point neighbors set
         for neighbor in points_neighbors[point]:
           points_neighbors[neighbor].remove(point)
           points_neighbors[neighbor].append(elem)
         points_neighbors[elem] += points_neighbors[point]
-
         del points_neighbors[point]
 
-        # Update polygons
-        for prim in point.prims():
-          if prim.type() == hou.primType.Polygon:
-            prim.addVertex(elem)
-        #geo.deletePoints([point])
-        marked_for_delete.append(point)
-
         affected_elems += old_point_edges + old_elem_edges + [point, elem]
-        affected_elems_points = []
-        for affected_elem in affected_elems:
-          for affected_elem_point in elems_to_points[affected_elem]:
-            if affected_elem_point != point_other:
-              affected_elems_points.append(affected_elem_point)
-          del elems_to_points[affected_elem]
-        
-        for affected_elems_point in affected_elems_points:
-          del points_to_elem[affected_elems_point]
-          if affected_elems_point not in marked_for_delete and affected_elems_points != point_other:
-            min_dist, min_elem = min_dist_and_elem(affected_elems_point, virtual_edges)
-            elems_to_points[min_elem].append(affected_elems_point)
-            points_to_elem[affected_elems_point] = min_elem
-            dist_to_pairs.put((min_dist, Pair(affected_elems_point, min_elem)))
-        break_point += 1
-geo.deletePoints(marked_for_delete)
+      # Update affected_elems
+      affected_elems_points = []
+      for affected_elem in affected_elems:
+        for affected_elem_point in elems_to_points[affected_elem]:
+          if affected_elem_point != point_other:
+            affected_elems_points.append(affected_elem_point)
+        del elems_to_points[affected_elem]
+      
+      for affected_elems_point in affected_elems_points:
+        del points_to_elem[affected_elems_point]
+        if affected_elems_point not in marked_for_delete_points and affected_elems_points != point_other:
+          min_dist, min_elem = min_dist_and_elem(affected_elems_point, virtual_edges)
+          #print((affected_elems_point.number(), (min_dist, min_elem)))
+          elems_to_points[min_elem].append(affected_elems_point)
+          points_to_elem[affected_elems_point] = min_elem
+          dist_to_pairs.put((min_dist, Pair(affected_elems_point, min_elem)))
+      break_point += 1
+geo.deletePrims(marked_for_delete_polys, keep_points=True)
+geo.deletePoints(marked_for_delete_points)
